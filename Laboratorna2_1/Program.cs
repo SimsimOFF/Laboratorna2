a3f44d8244e5293a0f695680933797eab9c99eaf
{{SimsimOFF}} <{{S.Melik-Kazaryan_FIT_11_21_B_d@knute.edu.ua}}>
2022-09-26 11:55:57 +0300

    first commit

---
+++ Laboratorna2_1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorna2_1
+{
+    class Adress
+    {
+        string index;
+        public string Index
+        {
+            set
+            {
+                index = value;
+            }
+            get
+            {
+                return index;
+            }
+        }
+        private string country;
+        public string Country
+        {
+            set
+            {
+                country = value;
+            }
+            get
+            {
+                return country;
+            }
+        }
+        string city;
+        public string City
+        {
+            set
+            {
+                city = value;
+            }
+            get
+            {
+                return city;
+            }
+        }
+        string street;
+        public string Street
+        {
+            set
+            {
+                street = value;
+            }
+            get
+            {
+                return street;
+            }
+        }
+        string house;
+        public string House
+        {
+            set
+            {
+                house = value;
+            }
+            get
+            {
+                return house;
+            }
+        }
+        string apartment;
+        public string Apartment
+        {
+            set
+            {
+                apartment = value;
+            }
+            get
+            {
+                return apartment;
+            }
+        }
+        class Program
+        {
+            static void Main()
+            {
+                Adress program = new Adress();
+                program.Index = "Index: 085600";
+                Console.WriteLine(program.Index);
+                Console.WriteLine(new string('-', 50));
+                program.Country = "Country: Ukraine";
+                Console.WriteLine(program.Country);
+                Console.WriteLine(new string('-', 50));
+                program.City = "City: Kyiv";
+                Console.WriteLine(program.City);
+                program.Street = "Street: Dimitrova";
+                Console.WriteLine(program.Street);
+                program.House = "House: 22";
+                Console.WriteLine(program.House);
+                program.Apartment = "Apartment: 3";
+                Console.WriteLine(program.Apartment);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ Laboratorna2_3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Laboratorna2_3
+{
+    class Employee
+    {
+        public string name;
+        public string surname;
+        public static string dateOfHire;
+        public OperateCost operationCost;
+
+
+        public Employee(string name, string surname, string dateOfHire, OperateCost opCost)
+        {
+            this.name = name;
+            this.surname = surname;
+            Employee.dateOfHire = dateOfHire;
+            this.operationCost = opCost;
+
+        }
+        public static double DiscoverGrade(string dateOfHire)
+        {
+            double dateValueForGrade = (DateTime.Now - DateTime.Parse(dateOfHire)).TotalDays;
+
+            if (dateValueForGrade >= 1825 && dateValueForGrade < 3650)
+                return 1.1;
+            else if (dateValueForGrade >= 3650)
+                return 1.25;
+            else
+                return 1.5;
+        }
+    }
+    abstract class OperateCost
+    {
+        public double salary;
+        public double tax;
+
+        public abstract void ApplyBonus(double bonus, double grade);
+        public abstract void ApplyTax();
+    }
+
+    class Noob : OperateCost
+    {
+        public override void ApplyBonus(double bonus, double grade)
+        {
+            salary = (10000 + 2 * bonus) * grade;
+        }
+
+        public override void ApplyTax()
+        {
+            tax = salary * 0.34;
+        }
+    }
+
+    class Casual : OperateCost
+    {
+        public override void ApplyBonus(double bonus, double grade)
+        {
+            salary = (100000 + 5 * bonus) * grade;
+        }
+        public override void ApplyTax()
+        {
+            tax = salary * 0.34;
+        }
+    }
+
+    class Overlord : OperateCost
+    {
+        public override void ApplyBonus(double bonus, double grade)
+        {
+            salary = (1000000 + 10 * bonus) * grade;
+        }
+        public override void ApplyTax()
+        {
+            tax = salary * 0.34;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperateCost oc = new Casual();
+            Employee emp = new Employee("Sergey", "Klychko", "19.07.1999", oc);
+            double grade = Employee.DiscoverGrade("19.07.1999");
+            Console.WriteLine("Name: {0}, Surname: {1}, Date of Hire: {2}, Position: {3}", emp.name, emp.surname, Employee.dateOfHire, emp.operationCost);
+            oc.ApplyBonus(25000, grade);
+            oc.ApplyTax();
+            Console.WriteLine("Salary: {0}, Tax: {1}", emp.operationCost.salary, emp.operationCost.tax);
+            Console.ReadKey();
+        }
+    }
+}
